de1b6753ed0249d35ad3d9c5ce06d06afe62891d
Baylee Snead <bayleesnead@outlook.com>
2020-04-08 14:08:06 -0400

    enqueued google fonts in child theme

--- wp-content/themes/castilo-child/functions.php
+++ wp-content/themes/castilo-child/functions.php
@@ -10,9 +10,19 @@
 * @since Castilo
 */
 
+
+
 // Enqueue scripts and styles
 function castilo_child_scripts(){
-	wp_enqueue_style( 'castilo-style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'castilo', get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'castilo-style' ));
 }
 add_action( 'wp_enqueue_scripts', 'castilo_child_scripts' );
+
+// Enqueue Google Fonts
+function wpb_add_google_fonts() {
+   wp_enqueue_style( 'wpb-google-fonts', 'https://fonts.googleapis.com/css?family=Karla|Rubik&display=swap', false );
+   }
+
+   add_action( 'wp_enqueue_scripts', 'wpb_add_google_fonts' );
+?>
